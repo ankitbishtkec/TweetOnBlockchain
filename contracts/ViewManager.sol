168cc42867320b11bf80fbfe1230621107d16866
Ankit Bisht <anbisht@microsoft.com>
2022-09-23 18:06:57 +0530

    added events emission on view's creation

--- contracts/ViewManager.sol
+++ contracts/ViewManager.sol
@@ -7,6 +7,9 @@ contract ViewManager
         address userAddress;
         uint viewCreationTimeInEpoch;
     }
+
+    // Declaring an event
+    event newViewAdded(string userView, address userAddress, uint viewCreationTimeInEpoch);  
     
     ViewModel[] views ;
     
@@ -14,6 +17,7 @@ contract ViewManager
     {
         ViewModel memory currView = ViewModel(input, msg.sender, block.timestamp );
         views.push(currView);
+        emit newViewAdded(currView.userView, currView.userAddress, currView.viewCreationTimeInEpoch);  
         return "success";
     }
 
